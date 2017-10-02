a521f09e2f09b01a7565549b47db9ebd0be261ee
Evan Munz <emunz14@gmail.com>
2017-10-02 13:19:54 -0500

    added multiples sum and average python practice

---
+++ multiples_sum_average.py
@@ -0,0 +1,13 @@
+for x in range (1, 1000):
+    if (x % 2 != 0):
+        print x
+
+for y in range(5,1000000):
+    if (y % 5 == 0):
+        print y
+
+
+a = [1,2,5,10,255,3]
+
+print sum(a)
+print sum(a)/len(a)
