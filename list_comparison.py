1468c30d3497704cc32ea56a9ca3e88acb9e5547
Evan Munz <emunz14@gmail.com>
2017-10-02 14:27:51 -0500

    added list comparison function

---
+++ list_comparison.py
@@ -0,0 +1,11 @@
+list_one = ['celery', 'bread', 'carrots', 'milk']
+list_two = ['celery', 'bread', 'carrots', 'cream']
+
+def list_comparison(x, y):
+    if(x == y):
+        print "These lists are the same"
+    else:
+        print "These lists are not the same"
+
+
+list_comparison(list_one, list_two)
