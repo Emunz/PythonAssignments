e19f50f53d37037f5a17387b3c1406c44d32fab0
Evan Munz <emunz14@gmail.com>
2017-10-03 12:14:14 -0500

    dictionary fundamentals

---
+++ PythonFundamentals/dictionary_fundamentals.py
@@ -0,0 +1,10 @@
+
+my_dictionary = {
+    'name': 'Evan',
+    'age': '26',
+    'country of birth': 'The United States',
+    'favorite language': 'Python'
+}
+
+for key, value in my_dictionary.iteritems():
+    print 'My ' + key + ' is ' + value
