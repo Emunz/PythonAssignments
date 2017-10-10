5c681bc3509c5812a41e198589510d0761750eb6
Evan Munz <emunz14@gmail.com>
2017-10-10 15:21:06 -0500

    Added the MySQL stuff

--- flask_fundamentals/ninja_gold/server.py
+++ flask_fundamentals/ninja_gold/server.py
@@ -9,32 +9,29 @@ app.secret_key = 'ThisIsSecret!'
 def index():
     if session.has_key('total_gold') == False:
         session['total_gold'] = 0
+    if session.has_key('activities') == False:
+        session['activities'] = []
     return render_template("index.html")
 
 @app.route('/process_money', methods=["POST"])
 def process_money():
-    if session.has_key('activities') == False:
-        session['activities'] = []
     if request.form['building'] == 'farm':
         gold = random.randrange(10,21)
         session['total_gold'] += gold
         session['log'] = 'Earned ' + str(gold) + ' gold from the farm! (' + str(datetime.datetime.now()) + ')'
         session['activities'].append(session['log'])
-        color = 'green'
         return redirect("/")
     if request.form['building'] == 'cave':
         gold = random.randrange(5,10)
         session['total_gold'] += gold
         session['log'] = 'Earned ' + str(gold) + ' gold from the house! (' + str(datetime.datetime.now()) + ')'
         session['activities'].append(session['log'])
-        color = 'green'
         return redirect("/")
     if request.form['building'] == 'house':
         gold = random.randrange(2,6)
         session['total_gold'] += gold
         session['log'] = 'Earned ' + str(gold) + ' gold from the house! (' + str(datetime.datetime.now()) + ')'
         session['activities'].append(session['log'])
-        color = 'green'
         return redirect("/")
     if request.form['building'] == 'casino':
         gold = random.randrange(-50,51)
@@ -42,11 +39,9 @@ def process_money():
         if gold < 0:
             session['log'] = 'Lost ' + str(abs(gold)) + ' gold at the casino! (' + str(datetime.datetime.now()) + ')'
             session['activities'].append(session['log'])
-            color = 'red'
         else: 
             session['log'] = 'Earned ' + str(gold) + ' gold from the casino! (' + str(datetime.datetime.now()) + ')'
             session['activities'].append(session['log'])
-            color = 'green'
         return redirect("/")
 
 
