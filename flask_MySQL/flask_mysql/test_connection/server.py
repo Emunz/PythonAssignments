177f421996611fe4c2d19252331c09106fc283fa
Evan Munz <emunz14@gmail.com>
2017-10-11 11:57:08 -0500

    added the friends database practice

---
+++ flask_MySQL/flask_mysql/test_connection/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, redirect, request
+# import the Connector function
+from mysqlconnection import MySQLConnector
+import datetime
+app = Flask(__name__)
+# connect and store the connection in "mysql" note that you pass the database name to the function
+mysql = MySQLConnector(app, 'mydb')
+# an example of running a query
+# print mysql.query_db("SELECT * FROM countries")
+print mysql.query_db("SELECT * FROM friends")
+
+@app.route('/')
+def index():
+    query = "SELECT * FROM friends"
+    friends = mysql.query_db(query)
+    print mysql.query_db("SELECT * FROM friends")
+    return render_template('index.html', all_friends = friends)
+
+@app.route('/create_friend', methods=["POST"])
+def create_friend():
+    name = request.form['name']
+    age = request.form['age']
+    created_at = datetime.datetime.now()
+    print name, age
+    mysql.query_db("INSERT INTO friends (name, age) VALUES ('{}', {})".format( name, age))
+    return redirect('/')
+
+
+
+
+app.run(debug=True)
